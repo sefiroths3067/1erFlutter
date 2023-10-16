7cd067f9b13a698fc32d67f22af12dee731c492f
Eduardo Alvarez <eduardo.alvarez.guzman@gmail.com>
2023-10-15 21:07:21 -0300

    reparacion

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'pincha en +:',
+              'toca el signo +:',
             ),
             Text(
               '$_counter',
